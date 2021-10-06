8f63155f02a9e2221bc57475aa78b139f159686a
jusung-c <62228433+jusung-c@users.noreply.github.com>
2021-10-06 21:51:14 +0900

    Add files via upload

---
+++ 15552_빠른 A+B/src/Main.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+
+        int[][] arr = new int[N][2];
+
+        for(int i=0; i<N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine()," ");
+            arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][1] = Integer.parseInt(st.nextToken());
+        }
+
+        for(int i=0; i<N; i++) {
+            bw.write(arr[i][0]+arr[i][1]+"\n");
+        }
+
+        bw.close();
+
+    }
+}
