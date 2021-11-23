621e0e755dd14b3a67ded81d99b12cbe3ba02f11
jusung-c <62228433+jusung-c@users.noreply.github.com>
2021-11-23 22:41:11 +0900

    Update Main.java

--- 21317_징검다리 건너기/src/Main.java
+++ 21317_징검다리 건너기/src/Main.java
@@ -1,13 +1,13 @@
 // 언어 : JAVA , (성공/실패) : 1/0 
-// 메모리 : 18652 KB , 시간 : 396 ms
+// 메모리 : 16304 KB , 시간 : 152 ms
 
 import java.io.*;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static int[][] jumps;
-    public static int N, K;
-    public static int answer = Integer.MAX_VALUE;
+    static int[][] jumps;
+    static int N, K;
+    static int answer = Integer.MAX_VALUE;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -33,7 +33,7 @@ public class Main {
         bw.close();
     }
 
-    static void go(int stone_num, int sum, boolean super_jump) {
+    public static void go(int stone_num, int sum, boolean super_jump) {
         // n번째 돌에 오면 최소 값 체크 후 종료
         if (stone_num == N) {
             answer = Math.min(answer, sum);
@@ -44,13 +44,19 @@ public class Main {
         if (stone_num > N) return;
 
         // 1단 점프
-        go(stone_num + 1, sum + jumps[stone_num][0], false);
+        if (stone_num+1 <= N) {
+            go(stone_num + 1, sum + jumps[stone_num][0], super_jump);
+
+        }
 
         // 2단 점프
-        go(stone_num + 2, sum + jumps[stone_num][1], false);
+        if (stone_num+2 <= N) {
+            go(stone_num + 2, sum + jumps[stone_num][1], super_jump);
+
+        }
 
         // 3단 점프를 사용하지 않았을 경우
-        if (!super_jump) {
+        if (!super_jump && stone_num+3 <=N) {
             go(stone_num + 3, sum + K, true);
         }
     }
