a1c97d96baed10fdf64dcf115e6b6bd84fa8aab4
jusung-c <62228433+jusung-c@users.noreply.github.com>
2022-02-04 02:27:40 +0900

    Add files via upload

---
+++ 9095_1, 2, 3 더하기/src/Main.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int T = Integer.parseInt(br.readLine());
+
+        for (int t = 0; t < T; t++) {
+            System.out.println(f(Integer.parseInt(br.readLine()))+" ");
+        }
+    }
+
+    static int f(int n) {
+        if (n == 1) {
+            return 1;
+        } else if (n == 2) {
+            return 2;
+        } else if (n == 3) {
+            return 4;
+        }
+
+        return f(n - 1) + f(n - 2) + f(n - 3);
+    }
+}
