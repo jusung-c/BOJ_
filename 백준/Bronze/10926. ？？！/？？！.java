aa9c1988e8a36415797bdc1f028d9e0785fe0366
이주성 <32183520@dankook.ac.kr>
2023-02-11 13:25:56 +0900

    [Bronze V] Title: ??!, Time: 132 ms, Memory: 14272 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringTokenizer st;
+
+    public static void init() throws IOException {
+        String s = br.readLine();
+
+        System.out.println(s+"??!");
+
+    }
+
+    private static void pro() {
+
+
+
+    }
+
+    public static void main(String[] args) throws Exception {
+        init();
+
+        pro();
+
+        br.close();
+        bw.close();
+    }
+}
